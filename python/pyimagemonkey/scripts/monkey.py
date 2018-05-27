efe1c412e1c2c6cee58a72f5d7e52859a121e2c3
BernhardB <schluchti@gmail.com>
2018-05-27 21:15:37 +0200

    [0.1] imagemonkey-libs: added option to list available labels

--- python/pyimagemonkey/scripts/monkey.py
+++ python/pyimagemonkey/scripts/monkey.py
@@ -19,24 +19,41 @@ def _split_labels(labels, delimiter):
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.DEBUG)
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--labels', help='labels that you want to train your model on', required=True)
-	parser.add_argument("--delimiter", help="label delimiter", default="|")
-	parser.add_argument("--detection_type", help="type (object_detection | image_classification)", default="image_classification")
+	parser = argparse.ArgumentParser(prog='PROG')
+	subparsers = parser.add_subparsers(help='', dest='command')
+
+	#add subparser for 'train'
+	train_parser = subparsers.add_parser('train', help='Train a model on some ImageMonkey labels')
+	train_parser.add_argument('--labels', help='labels that you want to train your model on', required=True)
+	train_parser.add_argument("--delimiter", help="label delimiter", default="|")
+	train_parser.add_argument("--detection_type", help="type (object_detection | image_classification)", default="image_classification")
+	
+	#add subparser for 'list-labels'
+	list_labels_parser = subparsers.add_parser('list-labels', help='List all the labels that are available at ImageMonkey')
+
 	args = parser.parse_args()
 
-	detection_type = None
-	if args.detection_type == "object_detection":
-		detection_type = Type.OBJECT_DETECTION
-	elif args.detection_type == "image_classification":
-		detection_type = Type.IMAGE_CLASSIFICATION
-	else:
-		print("Unknown object_detection type: %s" %(detection_type,))
-		sys.exit(1)
+	imagemonkey_api = API(api_version=1)
 
 
-	try:
-		tensorflow_trainer = TensorflowTrainer("/tmp/test", clear_before_start=True, tf_object_detection_models_path="/tensorflow_models/")
-		tensorflow_trainer.train(_split_labels(args.labels, args.delimiter), min_probability = 0.8, train_type = detection_type)
-	except Exception as e: 
-		print(e)
+	if args.command == "train":
+		detection_type = None
+		if args.detection_type == "object_detection":
+			detection_type = Type.OBJECT_DETECTION
+		elif args.detection_type == "image_classification":
+			detection_type = Type.IMAGE_CLASSIFICATION
+		else:
+			print("Unknown object_detection type: %s" %(detection_type,))
+			sys.exit(1)
+
+
+		try:
+			tensorflow_trainer = TensorflowTrainer("/tmp/test", clear_before_start=True, tf_object_detection_models_path="/tensorflow_models/")
+			tensorflow_trainer.train(_split_labels(args.labels, args.delimiter), min_probability = 0.8, train_type = detection_type)
+		except Exception as e: 
+			print(e)
+
+	if args.command == "list-labels":
+		labels = imagemonkey_api.labels()
+		for label in labels:
+			print(label)
