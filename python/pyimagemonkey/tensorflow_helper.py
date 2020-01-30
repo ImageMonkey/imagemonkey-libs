fc9720da52deb3ea12a866b2eb82ef60dccbe958
Bernhard B <schluchti@gmail.com>
2020-01-30 19:13:38 +0100

    add missing pagination to tensorflow_helper

--- python/pyimagemonkey/tensorflow_helper.py
+++ python/pyimagemonkey/tensorflow_helper.py
@@ -20,9 +20,9 @@ def get_available_tensorflow_releases(product_type=ProductType.TENSORFLOW):
 	releases = []
 	url = ""
 	if product_type == ProductType.TENSORFLOW:
-		url = "https://api.github.com/repos/tensorflow/tensorflow/releases"
+		url = "https://api.github.com/repos/tensorflow/tensorflow/releases?page=1&per_page=1000"
 	elif product_type == ProductType.TENSORFLOW_MODELS:
-		url = "https://api.github.com/repos/tensorflow/models/releases"
+		url = "https://api.github.com/repos/tensorflow/models/releases?page=1&per_page=1000"
 	else:
 		raise ImageMonkeyGeneralError("Invalid Tensorflow Product type")
 
