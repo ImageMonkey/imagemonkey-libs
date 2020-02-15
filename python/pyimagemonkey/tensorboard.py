2eb6522e845dd14662d285d070421ecd3af48b76
Bernhard B <schluchti@gmail.com>
2020-02-15 18:31:38 +0100

    improved TensorBoard screnshot funtionality

--- python/pyimagemonkey/tensorboard.py
+++ python/pyimagemonkey/tensorboard.py
@@ -13,23 +13,35 @@ class TensorBoard(object):
         self._path_to_tensorboard_screenshot = path_to_tensorboard_screenshot
         self._screenshot_output_dir = screenshot_output_dir
         self._process = None
+        self._started = False
 
         if not os.path.exists(path_to_tensorboard_screenshot):
             raise RuntimeError("tensorboard_screenshot.js doesn't exist at %s" %path_to_tensorboard_screenshot)
 
     def start(self):
-        logging.info("Starting tensorboard...")
-        self._process = subprocess.Popen(["tensorboard", "--logdir", self._path_to_log_dir])
+        logging.info("Starting TensorBoard...")
+        self._process = subprocess.Popen(["tensorboard", "--logdir", self._path_to_log_dir], 
+                                            stdout=subprocess.PIPE, universal_newlines=True, stderr=subprocess.STDOUT)
+        for line in iter(self._process.stdout.readline, ''):
+            logging.info("Waiting for TensorBoard to start...")
+            if "TensorBoard" in line: 
+                logging.info("TensorBoard started")
+                self._started = True
+                break
+            time.sleep(1)
+        
 
     def stop(self):
-        log.info("Stopping tensorboard")
+        logging.info("Stopping TensorBoard")
         os.kill(self._process.pid, signal.SIGINT)
 
     def screenshot(self):
-        log.info("Waiting 60sec before taking a screenshot...")
-        log.info("Take screenshot")
-        p = subprocess.Popen(["node", self._path_to_tensorboard_screenshot, "http://127.0.0.1:6006", self._screenshot_output_dir])
-        output, err = p.communicate()
-        return_code = p.returncode
-        if return_code != 0:
-            raise RuntimeError("Couldn't create snapshot: %s" %output) 
+        if self._started:
+            logging.info("Take Screenshot")
+            p = subprocess.Popen(["node", self._path_to_tensorboard_screenshot, "http://127.0.0.1:6006", self._screenshot_output_dir])
+            output, err = p.communicate()
+            return_code = p.returncode
+            if return_code != 0:
+                raise RuntimeError("Couldn't create snapshot: %s" %output)
+        else:
+            logging.info("Cannot take screenshot, as TensorBoard isn't started") 
